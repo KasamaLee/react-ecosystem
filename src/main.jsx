85e93ae37d7a312474f281f49a0130e65fe399a2
KasamaLee <k.kasama.lee@gmail.com>
2023-09-08 11:01:42 +0700

    global css

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-
-  const stylesObj = {
-    backgroundColor: isDarkMode ? 'black' : 'white',
-    color: isDarkMode ? 'white' : 'black',
-  };
-
-  const handleToggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
 
   return (
-    <div className='App' style={stylesObj} >
-      <h1>Theme App</h1>
-      <button onClick={handleToggleTheme}>Toggle Theme</button>
-    </div>
+    <h1>Vite</h1>
   );
 };
 
